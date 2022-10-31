afedaddc28912206724e1a4f2b05c2f5e06c18df
LucasWong02 <61992116+LucasWong02@users.noreply.github.com>
2022-10-31 09:36:32 -0600

    Initial Commit

---
+++ FrontProyecto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FrontProyecto
+//
+//  Created by Lucas Wong Mang on 10/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
