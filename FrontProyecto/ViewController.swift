1d6e08e2c1b5fa98cb5ca33b5b7e4a7db9a6c5d9
LucasWong02 <61992116+LucasWong02@users.noreply.github.com>
2022-11-02 13:47:03 -0600

    Login-SignIn Views

--- FrontProyecto/ViewController.swift
+++ FrontProyecto/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var topConstraintHeight: NSLayoutConstraint!
+    @IBAction func ShowSignInPop(_ sender: Any) {
+        topConstraintHeight.constant=0;
+        UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        print("Clicked")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        topConstraintHeight.constant=800;
     }
 
 
