3c303a51e0379168ca9d847f4c45377eac4893ab
MaxvG115 <maxivillegas115@gmail.com>
2022-11-16 17:29:38 -0600

    Stripe sdk integration with ruby backend

--- FrontProyecto/SceneDelegate.swift
+++ FrontProyecto/SceneDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  SceneDelegate.swift
-//  FrontProyecto
-//
-//  Created by Lucas Wong Mang on 10/31/22.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
--- FrontProyecto/View Controllers/ViewController.swift
+++ FrontProyecto/View Controllers/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  FrontProyecto
-//
-//  Created by Lucas Wong Mang on 10/31/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
