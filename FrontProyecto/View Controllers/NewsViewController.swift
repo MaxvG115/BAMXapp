6b420592e7183a0fa1438f110b5a91ef5b39ec26
LucasWong02 <61992116+LucasWong02@users.noreply.github.com>
2022-11-30 19:05:25 -0600

    CambioMax

--- FrontProyecto/View Controllers/NewsViewController.swift
+++ FrontProyecto/View Controllers/NewsViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class NewsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var ConfigButton: UIButton!
     @IBOutlet weak var cardTableView: UITableView!
     
     let pictures: [UIImage] = [UIImage(named: "Noticias1.png")!, UIImage(named: "Noticias2.png")!, UIImage(named: "Noticias3.png")!, UIImage(named: "Noticias4.png")!, UIImage(named: "Noticias5.png")!]
@@ -11,10 +12,15 @@ class NewsViewController: UIViewController, UITableViewDelegate, UITableViewData
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        ConfigButton.addTarget(self, action: #selector(tapOnButton), for: .touchUpInside)
         // Do any additional setup after loading the view.
     }
     
-    
+    @objc func tapOnButton(){
+        let story = UIStoryboard(name: "Main", bundle: nil)
+        
+        let controller = story.instantiateViewController(identifier: "") as! ConfigurationViewController
+    }
     ///How many rows in the table view
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pictures.count
--- FrontProyecto/View Controllers/ViewController.swift
+++ FrontProyecto/View Controllers/ViewController.swift
@@ -6,10 +6,15 @@
 //
 
 import UIKit
+import CoreNFC
 
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var AuthBtn: UIButton!
+    
+    @IBOutlet weak var ConfigBtn: UIButton!
+
     @IBOutlet weak var topConstraintHeight: NSLayoutConstraint!
     @IBAction func ShowSignInPop(_ sender: Any) {
         topConstraintHeight.constant=0;
@@ -21,9 +26,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        AuthBtn.layer.borderWidth=1
+        AuthBtn.layer.borderColor=UIColor.black.cgColor
+
         topConstraintHeight.constant=800;
     }
-
 
 }
 
